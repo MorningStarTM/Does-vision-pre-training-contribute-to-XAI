2e2677ad3ae7849ccbd5f92487ac363bb5de37c5
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-25 15:18:53 +0530

    residual decoder created

--- models/resnet.py
+++ models/resnet.py
@@ -61,3 +61,28 @@ def resEncoder(input_shape):
 
     encoder_model = Model(inputs, x, name='encoder')
     encoder_model.summary()
+
+
+
+def resDecoder(input_shape):
+    first_filter = 16
+    start = 16
+    end = 512
+    inputs = layers.Input(shape=input_shape)
+
+    x = layers.Conv2DTranspose(512, kernel_size=7, strides=1, padding='same')(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.LeakyReLU(alpha=0.2)(x)
+    x = layers.Conv2DTranspose(512, kernel_size=7, strides=1, padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.LeakyReLU(alpha=0.2)(x)
+
+    for value in range(end, start - 1, -1):
+        if (value & (value - 1)) == 0 and value <= end:
+            x = resnet_deconv_block(x, filters=value, strides=2)
+
+    x = layers.Conv2D(8, kernel_size=7, strides=1, padding='same', activation='relu')(x)
+    x = layers.Conv2D(3, kernel_size=7, strides=1, padding='same', activation='linear')(x)
+    decoder_model = Model(inputs=inputs, outputs=x)
+    decoder_model.summary()
+    
