714ba71024bc2b8384565a14cca398d0fceaf3d1
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-25 18:18:36 +0530

    residual autoencoder created

---
+++ models/autoencoder.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+from keras import layers as L
+from keras.models import Model
+from .resnet import resEncoder, resDecoder
+
+
+class ResAutoencoder:
+    def __init__(self, inputs: tuple):
+        self.encoder = resEncoder(inputs)
+        self.decoder = resDecoder(self.encoder.output)
+
+    def build_autoencoder(self):
+        autoencoder_inputs = self.encoder.input
+        encoded = self.encoder(autoencoder_inputs)
+        autoencoder_outputs = self.decoder(encoded)
+
+        autoencoder_model = Model(autoencoder_inputs, autoencoder_outputs, name='autoencoder')
+        return autoencoder_model
+
+    
