b5a0cc859b558adca7fd5da9de0c64d723020397
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-25 19:26:00 +0530

    vgg autoencoder created

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from keras import layers as L
 from keras.models import Model
 from .resnet import resEncoder, resDecoder
+from .vgg import vggEncoder, vggDecoder
 
 
 class ResAutoencoder:
@@ -17,4 +18,17 @@ class ResAutoencoder:
         autoencoder_model = Model(autoencoder_inputs, autoencoder_outputs, name='autoencoder')
         return autoencoder_model
 
+
+class VggAutoencoder:
+    def __init__(self, inputs: tuple):
+        self.encoder = vggEncoder(inputs)
+        self.decoder = vggDecoder(self.encoder.output)
+
+    def build_autoencoder(self):
+        autoencoder_inputs = self.encoder.input
+        encoded = self.encoder(autoencoder_inputs)
+        autoencoder_outputs = self.decoder(encoded)
+
+        autoencoder_model = Model(autoencoder_inputs, autoencoder_outputs, name='autoencoder')
+        return autoencoder_model
     
